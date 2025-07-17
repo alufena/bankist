74947786ad7ced06d4f43bc2869b531e3ae691de
alufena <andersonpolv@gmail.com>
2025-07-16 22:00:17 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -69,7 +69,15 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
+
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
 
 // console.log(containerMovements.innerHTML);
 
