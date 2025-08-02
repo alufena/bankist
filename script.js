32f1a06364f57efc058d79fd00b01a3afaa6fe4c
alufena <andersonpolv@gmail.com>
2025-08-01 23:04:12 -0300

    internationalizing dates

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const formatMovementDate = function (date) {
+const formatMovementDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
   const daysPassed = calcDaysPassed(new Date(), date);
@@ -103,10 +103,11 @@ const formatMovementDate = function (date) {
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
   else {
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0); // método é zero based, por isso o " + 1"
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    /*     const day = `${date.getDate()}`.padStart(2, 0);
+        const month = `${date.getMonth() + 1}`.padStart(2, 0); // método é zero based, por isso o " + 1"
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`; */
+    return new Intl.DateTimeFormat(locale).format(date)
   }
 };
 
@@ -124,7 +125,7 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
     /* const day = `${date.getDate()}`.padStart(2, 0);
     const month = `${date.getMonth() + 1}`.padStart(2, 0); // método é zero based, por isso o " + 1"
     const year = date.getFullYear();
@@ -218,12 +219,24 @@ btnLogin.addEventListener('click', function (e) {
     // console.log('Login');
     containerApp.style.opacity = 100;
     const now = new Date(); // cria a data atual
-    const day = `${now.getDate()}`.padStart(2, 0);
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      weekday: 'long',
+    };
+    // const locale = navigator.language; // usa o país de origem do navegador para exibição de datas
+    labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(
+      now
+    );
+    /* const day = `${now.getDate()}`.padStart(2, 0);
     const month = `${now.getMonth() + 1}`.padStart(2, 0);
     const year = now.getFullYear();
     const hour = `${now.getHours()}`.padStart(2, 0);
     const min = `${now.getMinutes()}`.padStart(2, 0);
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`; */
     inputLoginUsername.value = inputLoginPin.value = ''; // limpa os campos de login e senha ao logar com sucesso
     inputLoginPin.blur(); // retira o foco do campo de pin
     /*     displayMovements(currentAccount.movements);
